26a8538a285b93d2f09536b9126ce081ba6dded3
CHAOS2614 <330276680@qq.com>
2020-11-11 15:47:29 +0800

    ssh

--- mavendemo/src/main/java/cn/edu/bjfu/maven/mavendemo/MavendemoApplication.java
+++ mavendemo/src/main/java/cn/edu/bjfu/maven/mavendemo/MavendemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MavendemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("hello world");
         SpringApplication.run(MavendemoApplication.class, args);
     }
 
